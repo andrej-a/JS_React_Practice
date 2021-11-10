4a832df0f1b9d190b0577e98772590e7a771690f
github-usernameandrej-a <amelyanovich.andrej@mail.ru>
2021-11-10 11:52:04 +0300

    init repos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>TESTING</h2>
+};
+
+
+class Field extends React.Component {
+    render() {
+      const holders = "Write here."
+  const styles = {
+    width: "300px"
+  }
+  return <input
+  type = "text"
+  placeholder = {
+    holders
+  }
+  style = {
+    styles
+  }
+/>;
+    }
+}
+
+function Btn() {
+ const text = "Log in";
+ const logged = false;
+  return <button>{logged ? "Enter" : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Field></Field>
+        <Btn></Btn>
     </div>
   );
 }
-
 export default App;
